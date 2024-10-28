c4743ce3df34df60dabb225ed780de5108022251
yurymazzepadrum <y9853353903@yandex.ru>
2024-10-28 17:10:04 +0300

    Update DZ Рекурсия.py

--- DZ Рекурсия.py
+++ DZ Рекурсия.py
@@ -1,15 +1,12 @@
 #----------------------------------------------------------DZ Рекурсия
-# Напишите функцию get_multiplied_digits и параметр number в ней.
-
 def get_multiplied_digits(number):
-# Создайте переменную str_number и запишите строковое представление(str) числа number в неё.
-    str_number = '40203'
-# Основной задачей будет отделение первой цифры в числе: создайте переменную first и запишите в неё первый символ из str_number в числовом представлении(int)
-    first = 4
-# Возвращайте значение first * get_multiplied_digits(int(str_number[1:]))
-    return first * get_multiplied_digits(int(str_number[1:]))
-    if str_number == 0:
+    str_number = str(number) # Создайте переменную str_number и запишите строковое представление(str) числа number в неё.
+    first = int(str_number[0]) # создайте переменную first и запишите в неё первый символ из str_number в числовом представлении(int).
+    if len(str_number) > 1:
+        return  first * get_multiplied_digits(int(str_number[1:]))
+    else:
         return first
+
 
 
 
